d9abf8aa60e6ed62d095a854e5996a80f9789fba
fdx321 <736903875@qq.com>
2015-12-15 14:13:21 +0800

    修改ZhihuNotGenRequest.py的实现

--- DZhihuSpider/spiders/ZhihuNotGenRequest.py
+++ DZhihuSpider/spiders/ZhihuNotGenRequest.py
@@ -1,11 +1,13 @@
 # -*- coding: utf-8 -*-
 # coding: utf-8
-from scrapy import signals, Request
+from scrapy import signals, Request, FormRequest
 from scrapy.selector import Selector
-from scrapy.spiders import Spider
+from scrapy.spiders import Spider, CrawlSpider
 from scrapy.xlib.pydispatch import dispatcher
 from scrapy_redis import connection
 from scrapy.utils.project import get_project_settings
+from scrapy_redis.spiders import RedisMixin
+
 from DZhihuSpider.items import ZhihuspiderItem
 
 try:
@@ -16,26 +18,46 @@ except ImportError:
 QUEUE_KEY = 'Zhihu:requests'
 
 
-class ZhihuNotGenRequestSpider(Spider):
+class ZhihuNotGenRequestSpider(RedisMixin, CrawlSpider):
     name = "ZhihuNotGenRequest"
     allowed_domains = ['zhihu.com']
-    start_urls = ['http://www.zhihu.com/people/yu-kuang-76/about']
+    start_urls = ['http://www.zhihu.com/people/xiao-liu-95-62/about']
     host = 'http://www.zhihu.com'
 
     def __init__(self):
         settings = get_project_settings()
         self.queue_key = settings.get('SCHEDULER_QUEUE_KEY', QUEUE_KEY)
         self.server = connection.from_settings(settings)
-
-        dispatcher.connect(self.get_requests, signals.spider_opened)
+        self.headers = {
+            "Host": "www.zhihu.com",
+            "Connection": "keep-alive",
+            "Cache-Control": "max-age=0",
+            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+            "User-Agent": "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/34.0.1847.131 Safari/537.36",
+            "Referer": "http://www.zhihu.com/people/raymond-wang",
+            "Accept-Encoding": "gzip,deflate,sdch",
+            "Accept-Language": "zh-CN,zh;q=0.8,en-US;q=0.6,en;q=0.4,zh-TW;q=0.2",
+        }
+        self.cookies = {
+            '_za': r'bda5810c-88f0-40a8-8d2b-d9be0e0c58a9',
+            'q_c1': r'28f9a453b53a482486644378553c3a10|1447162001000|1447162001000',
+            '_xsrf': r'4307a4b2977f25efbdacbd89edf2e789',
+            'cap_id': r'"OThkOGIwMDVkMDllNGZmMzkzN2JkY2MzNzhhMmZjZWQ=|1448186640|774a87a7e0bd5ecec150a0d4bed38b570859c822"',
+            'z_c0': r'"QUFBQUF1VWdBQUFYQUFBQVlRSlZUUjBnZVZZM0ptcEVROU9YSzZ3bXpUUEJXQm0zSUkxSFl3PT0=|1448186653|1eb9dfd0eff895cab5c818fd97d103a17d557dfe"',
+            'unlock_ticket': r'"QUFBQUF1VWdBQUFYQUFBQVlRSlZUU1dhVVZhcmFDck02VUROeVV3c1oyRHQ1aWduQmVLYWdRPT0=|1448186653|c734f11184740390f0b34536e218952aabdcff46"',
+            '__utmt': r'1',
+            '__utma': r'51854390.16347795.1448186642.1448186642.1448186642.1',
+            '__utmb': r'51854390.18.10.1448186642',
+            '__utmc': r'51854390',
+            '__utmz': r'51854390.1448186642.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none)',
+            '__utmv': r'51854390.100-1|2=registration_date=20131118=1^3=entry_date=20131118=1'
+        }
         super(ZhihuNotGenRequestSpider, self).__init__()
 
-    def get_requests(self, spider):
-        if self.server.llen(self.queue_key) > 2000:
-            encoded_request = self.server.rpop(self.queue_key)
-            if encoded_request:
-                request = self.request_from_dict(pickle.loads(encoded_request))
-            self.crawler.engine.crawl(request, spider)
+    def start_requests(self):
+        for i, url in enumerate(self.start_urls):
+            yield FormRequest(url, meta={'cookiejar': i}, headers=self.headers, cookies=self.cookies,
+                              callback=self.parse_about)
 
     def request_from_dict(self, d):
         cb = getattr(self, 'parse_about')
@@ -54,9 +76,7 @@ class ZhihuNotGenRequestSpider(Spider):
 
     def parse_about(self, response):
         selector = Selector(response)
-
         item = ZhihuspiderItem()
-
         # http://www.zhihu.com/people/jiang-xiao-fan-92-14/about
         # 取jiang-xiao-fan-92-14作为userID
         item['userid'] = response.url.split('/')[-2]
@@ -83,3 +103,13 @@ class ZhihuNotGenRequestSpider(Spider):
             item['gender'] = u'unknown'
 
         yield item
+
+        while self.server.llen(self.queue_key) > 2000:
+            encoded_request = self.server.rpop(self.queue_key)
+            if encoded_request:
+                request = self.request_from_dict(pickle.loads(encoded_request))
+                if request._get_url().find('about') == -1:
+                    self.server.lpush(self.queue_key, encoded_request)
+                    continue
+            yield request
+            break
